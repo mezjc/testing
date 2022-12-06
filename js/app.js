5f1ea4e0cec060bacdf555970c96c8692d456358
Jhonnatan Meza <jhonnatanmc@gmail.com>
2022-12-05 23:33:11 -0500

    finalizado

--- js/app.js
+++ js/app.js
@@ -26,3 +26,32 @@ carrusel.addEventListener("mouseout",()=>{
 });
 
 start();
+
+
+let slider = document.querySelector(".slider-contenedor");
+let sliderIndividual = document.querySelectorAll(".contenido-slider");
+let contador = 1;
+let width = sliderIndividual[0].clientWidth;
+let intervalos = 7000;
+
+window.addEventListener("resize", function(){
+  width=sliderIndividual[0].clientWidth;
+});
+
+setInterval(function(){
+  slides();
+},intervalos);
+
+function slides(){
+  slider.style.transform = "translate("+(-width*contador)+"px)";
+  slider.style.transition="transform.7s";
+  contador++;
+
+  if(contador == sliderIndividual.length){
+    setTimeout(function(){    
+      slider.style.transform = "translate(0px)";
+      slider.style.transition="transform.0s";
+      contador=1
+    },1500)
+  }
+}
